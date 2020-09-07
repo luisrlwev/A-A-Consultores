c2edd534fd5501201774fda86da3d07b474b98c9
Luis Angel Hernández <rlwev@hotmail.com>
2020-09-07 09:39:15 -0500

    Primera version de mis cambios

---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+<!-- Start footer section
+	============================================= -->
+	<footer class="footer_copyright">
+		<div class="container">
+			<div class="footer_copyright_content">
+				<div class="row">
+					<div class="col-lg-4 col-md-12">
+						<div class="footer_social ul-li clearfix">
+							<ul>
+								<li><a href="https://www.facebook.com/AA-Consultores-Fiscales-109110437589836/"><i class="fab fa-facebook-f"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-12">
+						<div class="footer_logo text-center">
+							<a href="index"><img src="assets/img/logo/flogo-1.png" alt=""></a>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-12">
+						<div class="copyright_text text-right">
+							Copyright &copy;<script>
+								document.write(new Date().getFullYear());
+	                        </script> | <a href="https://marketingweb.com.mx/" target="_blank"> Marketing Web</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+<!-- End  footer section
+	============================================= -->
+
+	<!-- For Js Library -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jarallax.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/appear.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/circle-progress.js"></script>
+	<script src="assets/js/parallax-scroll.js"></script>
+	<script src="assets/js/jquery.counterup.min.js"></script>
+	<script src="assets/js/waypoints.min.js"></script>
+	<script src="assets/js/tilt.jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
+	<script src="assets/js/gmap3.min.js"></script>
+	<!-- Google Maps -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmvGDB4jFjNlVZHzFD6yhoGeIB3LeTD68&language=es&region=MX&callback=initMap" async defer></script>
+    <script src="assets/js/map.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>A&A Consultores Fiscales</title>
+	<link rel="shortcut icon" href="assets/img/logo/ficon.png" type="image/x-icon">
+	<meta name="author" content="Marketing Web">
+	<meta name="description" content="Rexlaw - Law Attorney HTML Template">
+	<meta name="keywords" content="	accountant, adviser, advocate, attorney, barrister, business, consultant, counsel, finance, justice, law, law firm, lawyer, legal, solicitor">
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<link rel="stylesheet" href="assets/css/fontawesome-all.css">
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/flaticon.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/video.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+	<div id="preloader"></div>
+	<div class="up">
+		<a href="#" class="scrollup text-center"><i class="fas fa-chevron-up"></i></a>
+	</div>
+
+<!-- Start of header section
+	============================================= -->
+	<header id="header_id" class="main_header header_style_one">
+		<div class="header_top">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="header_contact_info float-left ul-li">
+						<ul class="text-white">
+							<li><i class="far fa-envelope"></i> info@aaconsultoresfiscales.com</li>
+							<li><i class="fas fa-phone"></i> 333 170 2849</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="header_follow_social float-right ul-li">
+						<ul>
+							<li><a href="https://www.facebook.com/AA-Consultores-Fiscales-109110437589836/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /header_top -->
+		<div class="header_main_menu">
+			<div class="site_logo">
+				<a href="index"><img src="assets/img/logo/logo-1.png" alt=""></a>
+			</div>
+			<nav class="main_navigation ul-li">
+				<ul class="text-white">
+					<li><a href="index">Inicio</a></li>
+					<li><a href="bufete-de-abogados">Nosotros</a></li>
+					<li class="dropdown">
+						<a>Servicios</a>
+						<ul class="dropdown-menu clearfix">
+							<li><a href="asesoria-fiscal">Asesoria Fiscal</a></li>
+							<li><a href="asesoria-legal">Asesoria Legal</a></li>
+						</ul>
+					</li>
+					<li><a href="blog">Blog</a></li>
+					<li><a href="abogados">Contacto</a></li>
+				</ul>
+			</nav>
+			<!-- /nav Menu -->
+			<div class="wide_side_bar open_side_area clearfix">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="wide_side_inner">
+				<div class="side_overlay open_side_area"></div>
+				<div class="side_inner_content text-center">
+					<div class="close_btn open_side_area">Back <i class="fas fa-arrow-right"></i></div>
+					<div class="side_inner_logo">
+						<a href="!#"><img src="assets/img/logo/logo-1.png" alt=""></a>
+					</div>
+					<p>
+						BDLP is one of the leading multi-disciplinary law firms in Dhaka, Bangladesh managed by lawyers in and around Dhaka. The law firm is one of the top law firms in Dhaka and represents both foreign and local clients.
+					</p>
+					<div class="side_contact">
+						<div class="social_widget ul-li headline relative-position">
+							<h3> Follow Us On:</h3>
+							<ul>
+								<li class="h-fb"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li class="h-tw"><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li class="h-bh"><a href="#"><i class="fab fa-behance"></i></a></li>
+								<li class="h-yo"><a href="#"><i class="fab fa-youtube"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="side_copywright">
+						Copyright By @ Themexriver
+					</div>
+				</div>
+			</div>
+			<!-- side bar Inner -->
+			<div class="call_to_quote float-right">
+				<div class="call_icon float-left">
+					<i class="flaticon-call"></i>
+				</div>
+				<span>Consultas</span>
+				<span class="call_number">333 170 2849</span>
+				<div class="icon_bg">
+					<i class="flaticon-call"></i>
+				</div>
+			</div>
+			<!-- /call to action -->
+		</div>
+		<!-- /desktop version -->
+		<div class="mobile_menu">
+			<div class="mobile_menu_button open_mobile_menu">
+				<i class="fas fa-bars"></i>
+			</div>
+			<div class="mobile_menu_wrap">
+				<div class="mobile_menu_overlay open_mobile_menu"></div>
+				<div class="mobile_menu_content">
+					<div class="mobile_menu_close open_mobile_menu">
+						<i class="fas fa-window-close"></i>
+					</div>
+					<div class="m-brand-logo text-center">
+						<img src="assets/img/logo/logo-1.png" alt="">
+					</div>
+					<nav class="main-navigation  clearfix ul-li">
+						<ul id="main-nav" class="navbar-nav text-capitalize clearfix">
+							<li><a href="index">Inicio</a>
+							</li>
+							<li><a href="bufete-de-abogados">Nosotros</a></li>
+							<li class="dropdown">
+								<a>Servicios</a>
+								<ul class="dropdown-menu bg-azul">
+									<li><a href="asesoria-fiscal">Asesoria Fiscal</a></li>
+									<li><a href="asesoria-legal">Asesoria Legal</a></li>
+								</ul>
+							</li>
+							<li><a href="blog">Blog</a></li>
+							<li><a href="abogados">Contacto</a></li>
+						</ul>
+					</nav>
+					<div class="free_call_nm">
+						<div class="m_call_icon text-center">
+							<i class="flaticon-call"></i>
+						</div>
+						<div class="m_call_text">
+							<span>FREE CONSULTATION</span>
+							<strong>+987 (876) 754 8</strong>
+						</div>
+					</div>
+					<div class="m_social_area text-center ul-li">
+						<h3 class="text-white"> Síguenos en:</h3>
+						<ul>
+							<li class="m_fb"><a href="https://www.facebook.com/AA-Consultores-Fiscales-109110437589836/"><i class="fab fa-facebook-f"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /mobile menu -->
+	</header>
+<!-- End of header section ============================================= -->
